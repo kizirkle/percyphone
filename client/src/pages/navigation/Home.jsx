2c824d350d0eb7b853ae820d27fe5bd05d7c000a
kizirkle <kzrkle@gmail.com>
2026-09-03 11:23:02 -0400

    product component
    -created product component using a modified bootstrap card
    -added snipcart functionality
    -added props

--- client/src/pages/navigation/Home.jsx
+++ client/src/pages/navigation/Home.jsx
@@ -1,15 +1,19 @@
 //imports
-
+import Product from "../../components/Product";
 //react
 function Home() {
 
     return(
-        <>
             <main className="candy">
                 <p> hi im homepage</p>
+                <Product
+                id="1"
+                name="clown sticker"
+                description="a small clown sticker"
+                price="3"
+                image="https://i.etsystatic.com/38045685/r/il/38067c/7187222684/il_600x600.7187222684_s831.jpg"
+                />
             </main>
-            
-        </>
     )
 }
 //export

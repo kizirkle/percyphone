317ac862353462233217199df8804e50b07651ef
kizirkle <kzrkle@gmail.com>
2026-09-06 17:03:00 -0400

    solving merge conflicts

--- client/src/pages/navigation/Home.jsx
+++ client/src/pages/navigation/Home.jsx
@@ -7,12 +7,7 @@ function Home() {
     return(
             <main className="candy">
                 <ProductSection sectionTitle="New Arrivals!"/>
-<<<<<<< Updated upstream
-                <p> hi im homepage</p>
-                
-=======
                 <ProductSection sectionTitle="Hot Items!"/>
->>>>>>> Stashed changes
             </main>
     )
 }

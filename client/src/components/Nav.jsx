ba82e17c12d91deb3fb5ce28d6e1683c9969f909
kizirkle <kzrkle@gmail.com>
2026-09-06 15:44:06 -0400

    modularized the dropdown feature
    modularized Drop.jsx to allow dynamically added links to be added to each dropdown instead of a specific amount of links

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -8,37 +8,81 @@ function Nav() {
                 <div className="col-12 col-md-4 col-lg-1 my-1">
                     <Drop
                     dropTitle="Clowns"
-                    link1="/clowns#new"
-                    title1="new!"
-                    link2="/clowns#stickers"
-                    title2="stickers"
-                    link3="/clowns#more"
-                    title3="more"/>
+                    dropArray={
+                        [
+                            {
+                                title: "New Arrivals!",
+                                link: "/clowns#new"
+                            },
+                            {
+                                title: "Keychains",
+                                link: "/clowns#keychains"
+                            },
+                            {
+                                title: "Charms",
+                                link: "/clowns#charms"
+                            },
+                            {
+                                title: "Badges",
+                                link: "/clowns#badges"
+                            },
+                        ]
+                    }
+                    />
                 </div>
                 
                 <div className="col-12 col-md-4 col-lg-1 my-1">
                     <Drop
                     dropTitle="Items"
-                    link1="/shop#new"
-                    title1="new!"
-                    link2="/shop#stickers"
-                    title2="stickers"
-                    link3="/shop#more"
-                    title3="more"/>
+                    dropArray={
+                        [
+                            {
+                                title: "New Arrivals!",
+                                link: "/shop#new"
+                            },
+                            {
+                                title: "Keychains",
+                                link: "/shop#keychains"
+                            },
+                            {
+                                title: "Charms",
+                                link: "/shop#charms"
+                            },
+                            {
+                                title: "Badges",
+                                link: "/shop#badges"
+                            },
+                        ]
+                    }/>
                 </div>
                 <div className="col-12 col-md-4 col-lg-1 my-1">
                     <Drop 
                     dropTitle="Options"
-                    link1="/"
-                    title1="home"
-                    link2="/faq"
-                    title2="faq"
-                    link3="/about"
-                    title3="about"
-                    link4="/events"
-                    title4="events"
-                    link5="/login"
-                    title5="admin"/>
+                    dropArray={
+                        [
+                            {
+                                title: "home",
+                                link: "/"
+                            },
+                            {
+                                title: "faq",
+                                link: "/faq"
+                            },
+                            {
+                                title: "about",
+                                link: "/about"
+                            },
+                            {
+                                title: "events",
+                                link: "/events"
+                            },
+                            {
+                                title: "admin",
+                                link: "/login"
+                            },
+                        ]
+                    }
+                    />
                 </div>
                 
 

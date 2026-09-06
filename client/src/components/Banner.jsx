69e601355cd453d274b0ebc48ee70f7ef9d6377c
kizirkle <kzrkle@gmail.com>
2026-09-06 16:32:45 -0400

    fixed dropdown to fit website style

---
+++ client/src/components/Banner.jsx
@@ -0,0 +1,13 @@
+//imports
+
+//react
+function Banner(){
+
+    return(
+        <section>
+            
+        </section>
+    )
+}
+
+export default Banner;
--- client/src/pages/navigation/Home.jsx
+++ client/src/pages/navigation/Home.jsx
@@ -1,14 +1,13 @@
 //imports
-import Product from "../../components/Product";
+import Banner from "../../components/Banner";
 import ProductSection from "../../components/ProductSection";
 //react
 function Home() {
 
     return(
             <main className="candy">
+                <Banner/>
                 <ProductSection sectionTitle="New Arrivals!"/>
-                <p> hi im homepage</p>
-                
             </main>
     )
 }

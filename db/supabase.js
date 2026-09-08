680d2cc7953a45ec972a1e28484f6be6ba1d6a27
kizirkle <kzrkle@gmail.com>
2026-09-08 19:27:22 -0400

    editing env variables

--- db/supabase.js
+++ db/supabase.js
@@ -2,8 +2,8 @@ const { createClient } = require("@supabase/supabase-js");
 require("dotenv").config();
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SECRET_KEY,
+  process.env.TEST_SUPABASE_URL,
+  process.env.TEST_SUPABASE_SECRET_KEY,
   {
     db: {
       schema: "percyphone",

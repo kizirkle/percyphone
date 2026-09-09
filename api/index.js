2edd80fd5281a444968526e1fb0fec74b2df1052
kizirkle <kzrkle@gmail.com>
2026-09-09 11:04:06 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,14 +1,12 @@
 const express = require('express');
-const app = express();
+const router = express.Router();
 
 //const productRoutes = require('./Product');
 //const tagRoutes = require('./Tag');
 const adminRoutes = require('./Admin');
 
-app.use(express.json());
 
 //app.use('/products', productRoutes);
 //app.use('/tags', tagRoutes);
-app.use('/admin', adminRoutes);
+router.use('/admin', adminRoutes);
 
-module.exports = require('../server');

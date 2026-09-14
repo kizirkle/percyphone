747bb42579bb405633215028071efff264ebd3d4
kizirkle <kzrkle@gmail.com>
2026-09-14 18:04:31 -0400

    added thumbnail image to the preview images

--- api/router.js
+++ api/router.js
@@ -1,14 +1,16 @@
+//THIS CONTROLS WHERE EACH API ROUTE GOES.
+//imports
 const express = require('express');
 const router = express.Router();
-
+//routes
 const productRoutes = require('./Product');
 const tagRoutes = require('./Tag');
 const adminRoutes = require('./Admin');
 const taggedProductRoutes = require('./TaggedProduct');
-
+//use middleware to connect /routes to /api
 router.use('/product', productRoutes);
 router.use('/tag', tagRoutes);
 router.use('/admin', adminRoutes);
 router.use('/tagged_product', taggedProductRoutes);
-
+//export routes
 module.exports = router;
--- db/supabase.js
+++ db/supabase.js
@@ -1,6 +1,9 @@
+//THIS IS THE CONNECTION CLIENT TO SUPABASE. .ENV IS ALSO
+//REQUIRED HERE IN ORDER TO FUNCTION.
 const { createClient } = require("@supabase/supabase-js");
 require("dotenv").config();
 
+//create supabase connection
 const supabase = createClient(
   process.env.TEST_SUPABASE_URL,
   process.env.TEST_SUPABASE_SECRET_KEY,
@@ -11,4 +14,5 @@ const supabase = createClient(
   }
 );
 
+//export supabase
 module.exports = supabase;

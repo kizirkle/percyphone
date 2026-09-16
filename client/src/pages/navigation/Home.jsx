c828d524b431c1d62ca4da3de6927c5ca80e939e
kizirkle <kzrkle@gmail.com>
2026-09-15 20:51:37 -0400

    Update Home.jsx

--- client/src/pages/navigation/Home.jsx
+++ client/src/pages/navigation/Home.jsx
@@ -11,7 +11,7 @@ function Home() {
                     <div className="col-lg-6 col-12 carousel">
                         <Carousel className="">
                             <Carousel.Item>
-                                <img className="carousel-image" src="../../../public/wip.png"/>
+                                <img className="carousel-image" src="public/wip.png"/>
                                 <Carousel.Caption>
                                     {/*put text here if needed*/}
                                 </Carousel.Caption>
